669b16088637b8523743353772cc5f4b8d1de4f2
hoang.ho <panzer3553@gmail.com>
2018-01-03 17:26:17 +0700

    Fix duplilicate page on android

--- src/Screen.js
+++ src/Screen.js
@@ -1,5 +1,5 @@
 /*eslint-disable*/
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   NativeAppEventEmitter,
   DeviceEventEmitter,
@@ -24,22 +24,24 @@ class Navigator {
     this.navigatorEventHandler = null;
     this.navigatorEventHandlers = [];
     this.navigatorEventSubscription = null;
-    this._lastAction = {params: undefined, timestamp: 0};
+    this._lastAction = { params: undefined, timestamp: 0 };
   }
 
   _checkLastAction(params) {
-    if (Date.now() - this._lastAction.timestamp < 1000
-       && _.isEqual(params, this._lastAction.params)
-       && !params.force) {
-     return false;
+    if (
+      Date.now() - this._lastAction.timestamp < 1000 &&
+      _.isEqual(params, this._lastAction.params) &&
+      !params.force
+    ) {
+      return false;
     } else {
-      this._lastAction = {params, timestamp: Date.now()};
+      this._lastAction = { params, timestamp: Date.now() };
       return true;
     }
   }
 
   push(params = {}) {
-    if(!this._checkLastAction({method: 'push', passProps: params.passProps, screen: params.screen})) {
+    if (!this._checkLastAction({ method: 'push', screen: params.screen })) {
       return;
     }
     return NavigationSpecific.push(this, params);
@@ -90,7 +92,11 @@ class Navigator {
   }
 
   setButtons(params = {}) {
-    return platformSpecific.navigatorSetButtons(this, this.navigatorEventID, params);
+    return platformSpecific.navigatorSetButtons(
+      this,
+      this.navigatorEventID,
+      params
+    );
   }
 
   setTitle(params = {}) {
@@ -159,7 +165,9 @@ class Navigator {
 
   setOnNavigatorEvent(callback) {
     if (this.navigatorEventHandlers.length > 0) {
-      throw new Error('setOnNavigatorEvent can not be used after addOnNavigatorEvent has been called');
+      throw new Error(
+        'setOnNavigatorEvent can not be used after addOnNavigatorEvent has been called'
+      );
     }
     this.navigatorEventHandler = callback;
     this._registerNavigatorEvent();
@@ -167,22 +175,29 @@ class Navigator {
 
   addOnNavigatorEvent(callback) {
     if (this.navigatorEventHandler) {
-      throw new Error('addOnNavigatorEvent can not be used after setOnNavigatorEvent has been called');
+      throw new Error(
+        'addOnNavigatorEvent can not be used after setOnNavigatorEvent has been called'
+      );
     }
     if (this.navigatorEventHandlers.indexOf(callback) === -1) {
       this.navigatorEventHandlers.push(callback);
     }
     this._registerNavigatorEvent();
 
-    return () => this._removeOnNavigatorEvent(callback)
-    
+    return () => this._removeOnNavigatorEvent(callback);
   }
 
   _registerNavigatorEvent() {
     if (!this.navigatorEventSubscription) {
-      let Emitter = Platform.OS === 'android' ? DeviceEventEmitter : NativeAppEventEmitter;
-      this.navigatorEventSubscription = Emitter.addListener(this.navigatorEventID, (event) => this.onNavigatorEvent(event));
-      Navigation.setEventHandler(this.navigatorEventID, (event) => this.onNavigatorEvent(event));
+      let Emitter =
+        Platform.OS === 'android' ? DeviceEventEmitter : NativeAppEventEmitter;
+      this.navigatorEventSubscription = Emitter.addListener(
+        this.navigatorEventID,
+        event => this.onNavigatorEvent(event)
+      );
+      Navigation.setEventHandler(this.navigatorEventID, event =>
+        this.onNavigatorEvent(event)
+      );
     }
   }
 
@@ -228,7 +243,11 @@ class Screen extends Component {
   constructor(props) {
     super(props);
     if (props.navigatorID) {
-      this.navigator = new Navigator(props.navigatorID, props.navigatorEventID, props.screenInstanceID);
+      this.navigator = new Navigator(
+        props.navigatorID,
+        props.navigatorEventID,
+        props.screenInstanceID
+      );
     }
   }
 
@@ -240,7 +259,4 @@ class Screen extends Component {
   }
 }
 
-export {
-  Screen,
-  Navigator
-};
+export { Screen, Navigator };
